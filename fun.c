f8c6bad5068f2067a874e1bfe60242774f8e7372
Isaac Amponsah <Yawamp27@gmail.com>
2022-11-22 09:12:14 +0000

    func

---
+++ fun.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_c - prints character
+ * @args: character argument
+ * Return: number of characters
+ */
+int print_c(va_list args)
+{
+	int c;
+
+	c = va_arg(args, int);
+	return (_putchar(c));
+}
