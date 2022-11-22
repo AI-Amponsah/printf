c705c132cc6b1d6958db6158421c4b2f4e8fb5d1
Jhay-Great <alemonai.jenef@gmail.com>
2022-11-22 11:09:20 -0800

    main header

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef our_printf
+#define our_printf
+#include <stdio.h>
+#include <stdarg.h>
+/**
+  * struct specifier - struct specifier
+  * @valid: the valid character
+  * @f: the functions associated
+  */
+typedef struct specifier
+{
+	char *valid;
+	int (*f)(va_list);
+}spec;
+int _printf(const char *format, ...);
+
+
+#endif
