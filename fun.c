f4c0b4604db8b0cf3494940dd32162455b77f225
Isaac Amponsah <Yawamp27@gmail.com>
2022-11-22 09:15:34 +0000

    print string

--- fun.c
+++ fun.c
@@ -13,3 +13,25 @@ int print_c(va_list args)
 	c = va_arg(args, int);
 	return (_putchar(c));
 }
+/**
+ * print_s - prints a string
+ * @args: string  argument
+ * Return: number of characters
+ */
+int print_s(va_list args)
+{
+	int i, count = 0;
+	char *str;
+
+	i = 0;
+	str = va_arg(args, char*);
+	if (str == NULL)
+		str = "(null)";
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+		count++;
+	}
+	return (count);
+}
